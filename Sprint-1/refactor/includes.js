81ced7630782236167cf45e196a65b4b4909224a
reyhany <breyhanyildiz@gmail.com>
2025-11-06 20:40:17 +0100

    Sprint-1 finished

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,6 +1,17 @@
 // Refactor the implementation of includes to use a for...of loop
 
+
 function includes(list, target) {
+  for (const element of list) {
+    if(element === target){
+      return true;
+    }
+    
+  }
+  return false;
+}
+
+/*function includes(list, target) {
   for (let index = 0; index < list.length; index++) {
     const element = list[index];
     if (element === target) {
@@ -8,6 +19,6 @@ function includes(list, target) {
     }
   }
   return false;
-}
+}*/
 
 module.exports = includes;
